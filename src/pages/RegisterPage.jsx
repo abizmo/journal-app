de84d7e42a1600bc56433110878ac1dcc430a94c
abizmo <abizmo@gmail.com>
2021-09-21 12:13:03 +0100

    Add Style to Auth components

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => (
-  <div>
-    RegisterPage
-  </div>
+  <>
+    <h3 className="auth__title mb-4">Register</h3>
+    <form>
+      <input
+        autoComplete="none"
+        className="auth__input mb-2"
+        name="name"
+        placeholder="Your Name"
+        required
+        type="text"
+      />
+      <input
+        autoComplete="off"
+        className="auth__input mb-2"
+        name="email"
+        placeholder="tu@email.com"
+        required
+        type="email"
+      />
+      <input
+        className="auth__input mb-2"
+        name="password"
+        placeholder="TuContraseñ@"
+        required
+        type="password"
+      />
+      <input
+        className="auth__input mb-2"
+        name="confirm-password"
+        placeholder="TuContraseñ@"
+        required
+        type="password"
+      />
+      <button
+        className="btn btn-block btn-primary mb-2"
+        type="submit"
+      >Register</button>
+      <Link
+        className="link d-block"
+        to="/auth/login"
+      >Already register?</Link>
+    </form>
+  </>
 );
 
 export default RegisterPage;
--- src/routers/AuthRouter.jsx
+++ src/routers/AuthRouter.jsx
@@ -5,15 +5,19 @@ import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 
 const AuthRouter = () => (
-  <Switch>
-    <Route path="/auth/login">
-      <LoginPage />
-    </Route>
-    <Route path="/auth/register">
-      <RegisterPage />
-    </Route>
-    <Redirect to="/auth/login" />
-  </Switch>
+  <div className="auth__main">
+    <div className="auth__box-container">
+      <Switch>
+        <Route path="/auth/login">
+          <LoginPage />
+        </Route>
+        <Route path="/auth/register">
+          <RegisterPage />
+        </Route>
+        <Redirect to="/auth/login" />
+      </Switch>
+    </div>
+  </div>
 );
 
 export default AuthRouter;
