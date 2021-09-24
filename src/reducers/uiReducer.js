84be98afe5b9eb86929224d9f5bf5f68d6059461
abizmo <abizmo@gmail.com>
2021-09-24 08:46:31 +0100

    Add UI state (loading, error)

---
+++ src/reducers/uiReducer.js
@@ -0,0 +1,27 @@
+import { CLEAR_ERROR, SET_ERROR, START_ACTION, STOP_ACTION } from "../actions/uiActions";
+
+const initialState = {
+  loading: false,
+  error: null,
+};
+
+const uiReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case CLEAR_ERROR:
+      return { ...state, error: null }
+
+    case SET_ERROR:
+      return { ...state, error: payload }
+
+    case START_ACTION:
+      return { ...state, loading: true }
+
+    case STOP_ACTION:
+      return { ...state, loading: false }
+
+    default:
+      return state
+    }
+}
+
+export default uiReducer;
