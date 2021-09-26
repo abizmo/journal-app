76dbc884469044c866ba2291c86427adca287fc2
abizmo <abizmo@gmail.com>
2021-09-26 06:15:06 +0100

    Add check firebase auth

--- src/routers/JournalRouter.jsx
+++ src/routers/JournalRouter.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   BrowserRouter as Router,
   Redirect,
@@ -8,19 +9,44 @@ import {
 
 import AuthRouter from './AuthRouter';
 import JournalPage from '../pages/JournalPage';
+import { getCurrentUser } from '../firebase';
+import { login } from '../actions/authActions';
 
-const JournalRouter = () => (
-  <Router>
-    <Switch>
-      <Route path="/auth">
-        <AuthRouter />
-      </Route>
-      <Route exact path="/">
-        <JournalPage />
-      </Route>
-      <Redirect to="/auth/login" />
-    </Switch>
-  </Router>
-);
+const JournalRouter = () => {
+  const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true)
+  const [logged, setLogged] = useState(false)
+
+  useEffect(() => {
+    getCurrentUser((user) => {
+      if (user) {
+        const { displayName: name, uid: userId } = user
+        dispatch(login({ name, userId }))
+        setLogged(true)
+      } else {
+        setLogged(false);
+      }
+      setChecking(false);
+    });
+  }, [ dispatch ])
+
+  if (checking) {
+    return <h1>Loading...</h1>
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/auth">
+          <AuthRouter />
+        </Route>
+        <Route exact path="/">
+          <JournalPage />
+        </Route>
+        <Redirect to="/auth/login" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default JournalRouter;
