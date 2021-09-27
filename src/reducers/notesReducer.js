6d57084dc4fa97579ef6aa5ec234112ab8dae40f
abizmo <abizmo@gmail.com>
2021-09-27 06:44:31 +0100

    Add notes reducer

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  notes: [],
+  active: null,
+}
+
+const notesReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+
+    default:
+      return state
+  }
+}
+
+export default notesReducer;
