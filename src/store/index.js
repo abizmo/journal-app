ff02f2e4a9c932deb27b1b8b97b56771125f8779
abizmo <abizmo@gmail.com>
2021-09-27 06:44:31 +0100

    Add notes reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from 'redux-thunk';
 
 import authReducer from "../reducers/authReducer";
+import notesReducer from "../reducers/notesReducer";
 import uiReducer from "../reducers/uiReducer";
 
 const reducers = combineReducers({
   auth: authReducer,
+  notes: notesReducer,
   ui: uiReducer,
 });
 
