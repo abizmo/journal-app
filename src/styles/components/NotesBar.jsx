8396e78c9fca06fbe522d4d5f3ddbc0fb7f84efb
abizmo <abizmo@gmail.com>
2021-09-28 07:03:13 +0100

    Add Uploading Images

--- src/styles/components/NotesBar.jsx
+++ src/styles/components/NotesBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { saveNote } from '../../actions/notesActions';
+import { saveNote, uploadImage } from '../../actions/notesActions';
 
 const NotesBar = () => {
   const dispatch = useDispatch();
@@ -9,11 +9,33 @@ const NotesBar = () => {
     dispatch(saveNote());
   };
 
+  const handleAddImage = () => {
+    document.querySelector('#fileInput').click();
+  }
+
+  const handleUpload = (evt) => {
+    const file = evt.target.files[0];
+
+    if (file) {
+      dispatch(uploadImage(file));
+    }
+  }
+
   return (
     <div className="notes__bar-container">
       <p>19th March 2021</p>
+      <input
+        id="fileInput"
+        name="fileInput"
+        onChange={ handleUpload }
+        style={{ display: 'none' }}
+        type="file"
+      />
       <div>
-        <button className="btn btn-outline-white mr-1">Add Picture</button>
+        <button
+          className="btn btn-outline-white mr-1"
+          onClick={ handleAddImage }
+        >Add Picture</button>
         <button
           className="btn btn-white"
           onClick={ handleSave }
